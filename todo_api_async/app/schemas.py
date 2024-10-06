d695420a67e3d1275a4f53ec1db235bca5c8fa11
KupchinskyDev <kupchinsky.dv@zerolab.tech>
2024-10-06 11:31:30 +0300

    add validation to schemas.py

--- todo_api_async/app/schemas.py
+++ todo_api_async/app/schemas.py
@@ -1,29 +1,48 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
+from typing import Optional
 
 
+# Определение приоритетов через Enum
 class PriorityEnum(str, Enum):
     low = "low"
     medium = "medium"
     high = "high"
 
 
+# Модель для создания задачи
 class TaskCreate(BaseModel):
-    title: str
-    priority: PriorityEnum
+    title: str = Field(
+        ...,
+        max_length=100,
+        description="Название задачи, макс. длина - 100 символов")
+    priority: PriorityEnum = Field(
+        ...,
+        description="Приоритет задачи (low, medium, high)")
 
 
+# Модель для обновления задачи
 class TaskUpdate(BaseModel):
-    title: str
-    priority: PriorityEnum
-    completed: bool
+    title: Optional[str] = Field(
+        None,
+        max_length=100,
+        description="Название задачи, макс. длина - 100 символов")
+    priority: Optional[PriorityEnum] = Field(
+        None,
+        description="Приоритет задачи (low, medium, high)")
+    completed: Optional[bool] = Field(
+        None,
+        description="Статус выполнения задачи")
 
 
+# Модель для ответа (свойства задачи)
 class TaskResponse(BaseModel):
     id: int
     title: str
     priority: PriorityEnum
     completed: bool
 
+    # Настройки для работы с ORM
     class Config:
-        from_attributes = True
+        orm_mode = True  # Позволяет работать с ORM моделями
+        from_attributes = True  # Для ORM атрибутов (если нужно)
